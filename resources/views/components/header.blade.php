83ea718e1888b55c24ddc5874ce17a33b644e732
Nate Daly <justlikephp@gmail.com>
2023-05-17 22:47:08 +0100

    Adding views, tailwnd and course controller.

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,7 @@
+<di>
+    <div>
+        <img class="h-32 w-full object-cover lg:h-48"
+            src="https://www.internationalworkplace.com/application/files/5016/2564/3859/iosh-mohw-banner-v3.jpg"
+            alt="">
+    </div>
+</di>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>App Name - @yield('title')</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <x-header />
+    <div class="min-h-full">
+        @yield('content')
+    </div>
+</body>
+
+</html>
